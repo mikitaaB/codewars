f0e23c80b697fce9baf0b7ce8c0a2d09f3459a53
Mikita <nikita.492@yandex.by>
2023-04-20 14:02:27 +0300

    Refactor Counting_duplicates

--- 6 kyu/Counting_Duplicates.js
+++ 6 kyu/Counting_Duplicates.js
@@ -1,3 +1,6 @@
-function duplicateCount(text){
-    return (text.toLowerCase().split("").sort().join("").match(/([^])\1+/g) || []).length;
+function duplicateCount(text) {
+    const arrText = text.toLowerCase().split("");
+    const arrUnique = [...new Set(arrText)];
+    const arrCountEverySym = arrUnique.map(el => [el, arrText.filter(sym => sym === el).length]);
+    return arrCountEverySym.map(el => el[1]).filter(el => el > 1).length;
 }
